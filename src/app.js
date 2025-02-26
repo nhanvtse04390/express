02128b6ef0e2adb2cbdc70543c18f97b2e8efbba
nhanvt <nhanvt@mor.com.vn>
2025-02-26 15:26:09 +0700

    connect db

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require('mongoose');
 require("dotenv").config();
 
 const app = express();
@@ -7,6 +8,11 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('Failed to connect to MongoDB', err));
 
 // Routes
 // app.use("/api/users", require("./routes/userRoutes"));
