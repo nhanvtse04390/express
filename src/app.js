e822892dbdecb972e9659f4839214c9cb723e4a4
votrongnhan <nhanvt@mor.com.vn>
2025-03-02 23:39:08 +0700

    add CORS

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"], allowedHeaders: ["Content-Type", "Authorization"] }));
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
