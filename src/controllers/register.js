3c1dc246962849d27fcff39d23aee8d516bdc482
nhanvt <nhanvt@mor.com.vn>
2025-03-26 14:41:56 +0700

    add phone and address

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -2,16 +2,18 @@ const User = require('../models/User')
 const bcrypt = require("bcryptjs");
 
 exports.save =  async (req,res) => {
-	const {email} = req.body
-	const {username} = req.body
-	const {password} = req.body
+	const {email,username,password,phone} = req.body
+	const {} = req.body
 	const userAlready = await User.findOne({email})
 	const userNameAlready = await User.findOne({username})
+	const userPhoneAlready = await User.findOne({phone})
 	try {
 		if(userAlready) {
 			return res.status(409).json({status: 409,message: 'Tài khoản này đã tồn tại!'}) 
 		} else if(userNameAlready) {
 			return res.status(409).json({status: 409,message: 'Tên tài khoản này đã bị trùng!'})
+		} else if(userPhoneAlready) {
+			return res.status(409).json({status: 409,message: 'Số điện thoại này đã bị trùng!'})
 		} else {
 			const hashedPassword = await bcrypt.hash(password, 10)
 			const user = new User({...req.body,password:hashedPassword}) 
--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
 	username: { type: String, required: true, unique: true },
 	email: { type: String, required: true, unique: true },
+	phone:  { type: String, required: true, unique: true },
+	address:  { type: String, required: true },
 	password: { type: String, required: true },
 	createdAt: { type: Date, default: Date.now },
 	updatedAt: { type: Date, default: Date.now },
